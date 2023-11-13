402b746cd3dbb8363ce2d9626fd859f64863c63b
Mahinul <mahinulmostafa@gmail.com>
2023-11-13 17:47:13 +0600

    code refactored

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require( 'express' );
 const mongoose = require ( 'mongoose' );
-const app = express();
+const app = express( );
+module.exports = { app, mongoose };
 
-const dbConnect = require('./src/db_connection');
-const ShopModel = require ( './src/schema');
 
+
+
+
+
+const url = "mongodb+srv://himahinulabid:DjYFI1UPxb5BRyJl@cluster0.kgeats8.mongodb.net/Knigi?retryWrites=true&w=majority";
+
+
+const dbConnect = async ( ) => {
+    try {
+        mongoose.connect(
+            url
+            // the url parser is deprecated, not using it anymore
+        );
+    }
+    catch ( err ) {
+        console.log( err );
+    }
+    
+}
 
 // DB connect
-dbConnect()
-.then( (resolve ) =>{
+dbConnect( )
+
+.then( ( ) =>{
     console.log("Log: Succesfully Connected to the Database \n");
 })
+
 .catch ( ( err ) => {
     console.log(err);
 });
@@ -19,18 +39,15 @@ dbConnect()
 
 
 
-
-
-
 // sample data to be inserted in database
-const testInsert = new ShopModel({
-    productTitle: 'Woven Issue Four',
-    productPrice: {
-        regularPrice: 20,
-    },
-    productAboutInfo: 'Issue 4 explores the paradoxes at play in our world: death and rebirth, old and new, past and future. We take on a mindset of endless growth and the many catalysts we encounter in the creative process: setbacks, loss, imagination, and serendipity. From poetry to plants, this issue looks intently into the face of the immeasurable forces that propel us forward, and meditates on our role in designing the future. From Manly, Australia to Victoria, British Columbia, from Long Beach, California to Jersey City, New Jersey, we share the stories of nine artists, designers, and entrepreneurs who are holding the tension between the past, present, and future. You’ll meet modern day poet Joekenneth Museau who channelled the loss of his mother to cancer into a guidebook for grief, Julianne Ahn who took up ceramics as a meditation, and photographer Cody Cobb whose solo expeditions take us back in time. We study the cosmology of city planning at Arcosanti, an experimental architecture site in the Arizona desert, and glimpse the mythical symbols of Antelope Canyon through the eyes of Navajo guides.',
-    productSpecs : '144 pages, offset-printed and perfect bound, full color on uncoated and coated paper. Printed in Germany. Dimensions: 8.5 x 11.3 in'
-});
+// const testInsert = new ShopModel({
+//     productTitle: 'Woven Issue Four',
+//     productPrice: {
+//         regularPrice: 20,
+//     },
+//     productAboutInfo: 'Issue 4 explores the paradoxes at play in our world: death and rebirth, old and new, past and future. We take on a mindset of endless growth and the many catalysts we encounter in the creative process: setbacks, loss, imagination, and serendipity. From poetry to plants, this issue looks intently into the face of the immeasurable forces that propel us forward, and meditates on our role in designing the future. From Manly, Australia to Victoria, British Columbia, from Long Beach, California to Jersey City, New Jersey, we share the stories of nine artists, designers, and entrepreneurs who are holding the tension between the past, present, and future. You’ll meet modern day poet Joekenneth Museau who channelled the loss of his mother to cancer into a guidebook for grief, Julianne Ahn who took up ceramics as a meditation, and photographer Cody Cobb whose solo expeditions take us back in time. We study the cosmology of city planning at Arcosanti, an experimental architecture site in the Arizona desert, and glimpse the mythical symbols of Antelope Canyon through the eyes of Navajo guides.',
+//     productSpecs : '144 pages, offset-printed and perfect bound, full color on uncoated and coated paper. Printed in Germany. Dimensions: 8.5 x 11.3 in'
+// });
 
 
 // testInsert.save()
@@ -39,63 +56,11 @@ const testInsert = new ShopModel({
 //     console.log(resolve);
 // })
 
-// fetching data from MongoDB
-const b = async (criteria) => {
-    try {
-        let m =await ShopModel.find(
-            criteria // this takes an object
-        )
-        return m;
-    }
-    
-    catch (error) {
-        console.log(error);
-    }
-} // .then() .catch () to catch the result
-
-
-
-ShopModel.updateOne( // async functionalities
-    {
-        productTitle: "Woven Issue Four",
-    },
-    {
-        // what do i wanna update
-        productPrice: {
-            regularPrice: 30
-        },
-        productTitle:"Woven testing"
-    },
-) // then() and catch(){}
 
 
 
 
 
-
-
-
-
-
-
-//  ROUTING SECTION
-app.get('/api/v1/shoplist', ( req, res )=>{
-    // plannig to get all Shoplist from the database
-
-    b({/* getting all shop item*/})
-
-    .then( (result ) =>{
-        // res.status(200).contentType("text/json");
-        res.send(result);
-    })
-    
-    .catch ( (err) => {
-        console.log(err);
-    })
-})
-
-
-
-app.listen(8000, ()=>{
-    console.log("successfully hosted")
-});
+// ROUTE SETTING
+require('./src/routes/shopRoute');
+// its like copy and paste 
