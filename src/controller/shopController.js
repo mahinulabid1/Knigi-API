de29a7df57ac4f69a31a8a36c141bc654f7f87d0
Mahinul <mahinulmostafa@gmail.com>
2023-11-15 18:27:55 +0600

    added insert, update & delete operation

--- src/controller/shopController.js
+++ src/controller/shopController.js
@@ -1,16 +1,13 @@
-const ShopModel = require ( '../model/shopModel' );
-const { mongoose } = require ( '../../index' ); 
-
 // controller fetches model 
 //and do data operations
 
+const ShopModel = require ( '../model/shopModel' );
+const { mongoose } = require ( '../../index' ); 
+
 const getAllShopItem = async () => {
+
     try{
-        const result = await ShopModel.find(
-            {
-                // find all
-            }
-        )
+        const result = await ShopModel.find( { /* find all */ } );
         return result;
     }
     
@@ -21,4 +18,54 @@ const getAllShopItem = async () => {
 }
 
 
-module.exports = { getAllShopItem };
+const getItemById  = async ( id ) => {
+
+    try{
+        const result = await ShopModel.findById(id);
+        return result;
+    }
+    catch ( err ) {
+        console.log(err);
+    }
+
+}
+
+
+const insertItem = async ( data ) => { // data has to be an object
+
+    try {
+        const testInsert = new ShopModel( data );
+        testInsert.save( );
+    }
+
+    catch ( err ) {
+        console.log(err);
+    }
+    
+}
+
+// UPDATE SHOP ITEM
+const updateById = async (id, data, option) => {
+    if( option === undefined) {
+        option = null;
+    }
+
+    await ShopModel.findByIdAndUpdate( id, data, option);
+    
+}
+
+
+//DELETE SHOP ITEM
+const deleteById= async ( id ) => {
+    try {
+        await ShopModel.deleteOne({_id: id}) // {"_id": id} this "" is wrong, I kept getting error
+    }
+    
+    catch ( err ){ 
+        console.log(err);
+    }
+}
+
+
+
+module.exports = { getAllShopItem, getItemById, insertItem, updateById, deleteById };
--- src/routes/shopRoute.js
+++ src/routes/shopRoute.js
@@ -1,21 +1,72 @@
-// const { get } = require ( 'mongoose' );
-const { app } = require ( '../../index' );
-const { getAllShopItem } = require ( '../controller/shopController');
+const { express, app, router } = require ( '../../index' );
+const { getAllShopItem, getItemById, insertItem, updateById, deleteById } = require ( '../controller/shopController');
+app.use(express.json());
 
 
-// send all shop item list
-app.get( '/api/v1/shoplist' , ( req, res ) => {
+//default routing
+app
+    .get ( "/", ( req, res ) => { 
+        res.status ( 200 ).send ( "This is Homepage" );
+})
 
-    getAllShopItem()
 
-    .then( ( resolve ) =>{
-        res.status( 200 ).json( resolve );
+// GET ALL SHOP ITEM OR SINGLE ITEM BY ID
+app
+    .get( '/api/v1/shoplist' ,async ( req, res ) => {
+        let query = req.query;
+
+        try{
+
+            // send all shop item list
+            if(query.id === undefined){
+                const data = await getAllShopItem();
+                res.status( 200 ).json( data );
+            }
+
+            //ask for specific item with ID
+            else if( query.id !== undefined) {
+                // route : http://localhost:8000/api/v1/shoplist?id=655087298290bc43b3f580a7  (mongodbID)
+                const data = await getItemById(query.id);
+                res.status( 200 ).json( data );
+            }
+
+        } catch( err ) {
+            console.log( err );
+        }
     }) 
-
-    .catch( ( err ) =>{
-        console.log( err );
+    
+    //UPDATE SHOP SPECIFIC ITEM
+    .patch("/api/v1/shoplist", async (req, res) => {
+        try{
+            await updateById(req.query.id, req.body);
+            res.status(200).send("updated");
+        }
+        catch (err){ 
+            console.log(err)
+        }
     })
 
-});
+
+
+// CREATE NEW SHOP ITEM
+app
+    .post( "/api/v1/newShopItem", ( req, res ) =>{  
+        // console.log(req.body);
+        insertItem ( req.body ).then ( ( ) => { console.log( "item inserted" ) } );
+        res.status(200).contentType('application/json').send({message : "Data Successfully Inserted"});
+
+})
+
+
+app
+    .delete("/api/v1/shoplist", async (req , res) => {
+        await deleteById(req.query.id);
+        res.status(200).send(`Deleted ${req.query.id}`);
+    })
+
+
+
 
 app.listen( 8000 );
+
+
