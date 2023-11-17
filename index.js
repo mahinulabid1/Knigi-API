022ea2ac373f3c04fc1c2c247178f7bc9364188c
Mahinul <mahinulmostafa@gmail.com>
2023-11-17 22:18:51 +0600

    added unique key generator for image

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require( 'express' );
 const mongoose = require ( 'mongoose' );
 const router = express.Router( );
+const bodyParser = require('body-parser');  // didn't find any use yet
+const AWS = require( 'aws-sdk' );
+const multer = require('multer');
 const app = express( );
 app.use(express.json());
 
-module.exports = { express, app, mongoose, router };
+const upload = multer({dest: 'upload/'});
+
+
+// Set up AWS configuration
+AWS.config.update({
+  accessKeyId: 'AKIAUYPP6YV4XQS665GF',
+  secretAccessKey: '9TND7wbQ8GCMeAFSOvPNrz0+k1gGdrZHJ2IHMizT',
+  region: 'ap-south-1' // Change to your AWS region
+});
+
+// initialize s3
+const s3 = new AWS.S3();
+
+
+// unique key generator
+function generateUniqueKey() {
+    const timestamp = Date.now().toString(36); // Convert current timestamp to base 36 string
+    const randomStr = Math.random().toString(36).substr(2, 10); // Random string
+    const hash = Math.random().toString(36).substr(2, 10); // Hash of random number
+    
+    return `${timestamp}-${randomStr}-${hash}`;
+}
+
+module.exports = { express, app, mongoose, router, s3, generateUniqueKey, bodyParser, upload };
 
 const url = "mongodb+srv://himahinulabid:DjYFI1UPxb5BRyJl@cluster0.kgeats8.mongodb.net/Knigi?retryWrites=true&w=majority";
 
@@ -21,6 +47,7 @@ const url = "mongodb+srv://himahinulabid:DjYFI1UPxb5BRyJl@cluster0.kgeats8.mongo
         console.log( err );
     }
 })();
+
 
 
 // ROUTE SETTING
