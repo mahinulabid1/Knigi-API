db2e3f7c64b2d3bc6cf802825bd5275469541a28
Mahinul <mahinulmostafa@gmail.com>
2023-11-18 11:12:49 +0600

    created AWS_S3 module

---
+++ src/AWS_S3/FileController.js
@@ -0,0 +1,35 @@
+const { generateUniqueKey, s3 } = require ( '../../index' ); 
+
+// function : uploads image to the AWS S3 bucket
+const imageInput = async ( imageData ) => {
+
+    try{
+        const uniqueKey = generateUniqueKey();
+
+        s3.putObject({
+            Body: imageData,
+            Key : `shopItem/${uniqueKey}.jpg`,
+            Bucket: 'knigiimagedb'
+        }, 
+        (err, data) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
+                console.log("image uploaded successfully ");
+            }
+        })
+
+        return uniqueKey;
+    } 
+    
+    catch ( err ) {
+        console.log( err ) ;
+    }
+}
+
+
+
+
+
+module.exports = { imageInput }
