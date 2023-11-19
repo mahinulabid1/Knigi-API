f670689fc117e9eba5f60dd5dcd24eb623b55c3b
Mahinul <mahinulmostafa@gmail.com>
2023-11-19 14:28:28 +0600

    enabled ACL:public-read, removed BuffertoUrl convertion function

--- src/AWS_S3/FileController.js
+++ src/AWS_S3/FileController.js
@@ -1,21 +1,5 @@
 const { generateUniqueKey, s3 } = require ( '../../index' ); 
 
-// function: takes an image array buffer and return Blog URL
-const BlogUrlGenerator = async ( ArrayBufferOfImage ) => {
-    // Assuming you have a buffer called 'imageBuffer'
-    const imageBuffer = ArrayBufferOfImage// Your Node.js buffer containing the image data
-
-    // Convert the buffer to a Base64-encoded string
-    const base64Image = Buffer.from(imageBuffer).toString('base64');
-
-    // Create a data URL
-    const imageUrl = `data:image/jpeg;base64,${base64Image}`;
-
-    console.log(imageUrl);
-
-    return  imageUrl;
-} 
-
 
 // function : uploads image to the AWS S3 bucket
 const imageInput = async ( imageData ) => {
@@ -26,7 +10,8 @@ const imageInput = async ( imageData ) => {
         s3.putObject({
             Body: imageData,
             Key : `shopItem/${uniqueKey}.jpg`,
-            Bucket: 'knigiimagedb'
+            Bucket: 'knigiimagedb',
+            ACL: "public-read",
         }, 
         (err, data) => {
             if(err) {
