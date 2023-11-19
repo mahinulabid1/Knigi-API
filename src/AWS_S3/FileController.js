09cda4c31b241063320fd15363b68ff52bdfa170
Mahinul <mahinulmostafa@gmail.com>
2023-11-19 15:55:12 +0600

    modified fileUpload & added s3 deleteFile function

--- src/AWS_S3/FileController.js
+++ src/AWS_S3/FileController.js
@@ -31,6 +31,27 @@ const imageInput = async ( imageData ) => {
 }
 
 
+const deleteImageFile = async ( imageSource ) => {      // imageSource must contain path to file, including filename
+
+    try {
+        s3.deleteObject({
+            Key : imageSource, 
+            Bucket: 'knigiimagedb',
+        }, 
+        (err, data) => {
+            if(err) {
+                console.log(err);
+            }
+            else{
+                console.log("Log: Image deleted successfully");
+            }
+        })
+    }
+    
+    catch ( err ) {
+        console.log( err );
+    }
+}
 
 
-module.exports = { imageInput }
+module.exports = { imageInput, deleteImageFile }
