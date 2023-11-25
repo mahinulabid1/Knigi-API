67f4a94debe7b7a5eb7bf4977d6e58a7265f563f
Mahinul <mahinulmostafa@gmail.com>
2023-11-25 14:38:01 +0600

    added debug with console function

--- debug.js
+++ debug.js
@@ -1,7 +1,22 @@
 class Debugging {
-    
 
-    
+    console ( data ) {
+        // data should be an object
+        // it will tested by logging into the console
+        /*
+            {
+                FunctionName : "String: the function you're in",
+                DataFromRequest : req.body,
+                passwordFromForm: req.body.data
+            }
+        */
+       console.log("\n\nDEBUG RESULT:\n-----------------------")
+        for( const prop in data ) {
+            console.log(`From ${prop} :`);
+            console.log(`${data [ prop ]}\n`);
+        }
+    }
+
 }
 
 
