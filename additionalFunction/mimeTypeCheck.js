673237931fb0c3dccfb5a1964440316cc41f63f4
Mahinul <himahinulabid@gmail.com>
2023-12-29 23:02:38 +0600

    create file type checker module

---
+++ additionalFunction/mimeTypeCheck.js
@@ -0,0 +1,18 @@
+/*
+  - checks is the uplaoded files are image or not
+  - returns, if it is image = true
+  - if not = false
+*/
+const mimeTypeCheck = (mimeTypeArr) => {
+  for(let i = 0 ; i < mimeTypeArr.length; i++ ) {
+    let valid = mimeTypeArr[i].includes('image')
+    if(valid === false) {
+      return false;
+    }
+    else if(i === mimeTypeArr.length-1){
+      return true;
+    }
+  }
+}
+
+module.exports = { mimeTypeCheck };
