d832cdf14294accb9f7f21c1df0d8574ffd9b6b4
Mahinul <himahinulabid@gmail.com>
2023-12-31 21:58:22 +0600

    modify middleware

---
+++ src/middleware/shop/create/formFileHandler.js
@@ -0,0 +1,16 @@
+const { app } = require('@index');
+const imageUpload = require('@controller/shopController/fileUploadController.js')
+
+app.post("/api/v1/newShopItem", async (req, res, next) => {
+
+   try{
+      await imageUpload(req);
+      next();
+   }catch(err) {
+      res.status(400).json({
+         upload_status: 'failed',
+         details: err
+      })
+   }
+
+})
---
+++ src/middleware/shop/create/uploadHandler.js
@@ -0,0 +1,23 @@
+/*
+- the first middleware of shop
+- it has to be at top of all shop middleware in ../main.js file.
+*/
+
+const { app } = require("@index");
+const { upload } = require('@multer.config');
+
+const fileFields = [
+  {
+    name: 'bookPicture',
+    maxCount: 1
+  },
+  {
+    name: 'thumbnail',
+    maxCount: 1
+  }
+]
+
+app.post("/api/v1/newShopItem", upload.fields(fileFields), async (req, res, next) =>{
+  // I can only use upload.fields(fileFields) once for each request. can't use twice.
+  next();
+})
