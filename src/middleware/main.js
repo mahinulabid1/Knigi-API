9cbe1add76634da31badada7e7c7e6ed6c106abf
Mahinul <himahinulabid@gmail.com>
2023-12-31 21:58:59 +0600

    modify middleware main file

--- src/middleware/main.js
+++ src/middleware/main.js
@@ -1,9 +1,9 @@
 /*
 - middleware must maintain this sequence
 */
-// require('./bodyParser');
+
 
 // shop middleware
-require('./shop/uploadHandler')
-require('./shop/formFileHandler');
-require('./shop/formDataParser');
+require('./shop/create/uploadHandler')
+require('./shop/create/formFileHandler');
+require('./shop/create/formDataParser');
