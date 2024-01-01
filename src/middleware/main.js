6814ee938e02959f4d1b2f4522762b74ff5b0cf1
Mahinul <himahinulabid@gmail.com>
2024-01-01 21:52:31 +0600

    add update-middleware

--- src/middleware/main.js
+++ src/middleware/main.js
@@ -4,6 +4,12 @@
 
 
 // shop middleware
+// create new shop item
 require('./shop/create/uploadHandler')
 require('./shop/create/formFileHandler');
 require('./shop/create/formDataParser');
+
+// update shop item
+require('./shop/update/uploadHandler')
+require('./shop/update/formDataParser')
+
