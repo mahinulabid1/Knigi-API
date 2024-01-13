d248ddb3e9e1096ec194b5b7b88f5d636bbcd288
Mahinul <himahinulabid@gmail.com>
2024-01-13 22:01:50 +0600

    code refactor

--- src/controller/shopController/updateController.js
+++ src/controller/shopController/updateController.js
@@ -1,15 +1,37 @@
 const shopModel = require('@model/shopModel');
+const catchAsync = require('@utils/catchAsync');
+const AppError = require('@utils/appError');
+const multer = require('multer')
+const upload = multer({
+   dest: 'upload/'
+})
 
-const updateShopItem = async (req) => {
-   try{
-      const id = req.query.id;
-      console.log(id);
-      const data = req.body.knigi_parsedData;
-      await shopModel.findByIdAndUpdate(id, data );
-   }
-   catch(err) {
-      throw new Error(err);
-   }
-}
+exports.multerUpload = upload.none();
 
-module.exports = updateShopItem;
+exports.parseBody = catchAsync(async(req, res, next) => {
+   const parsedData = {
+      productTitle: req.body.productTitle,
+
+      productPrice: {
+         regularPrice: req.body.regularPrice,
+         discountedPrice: req.body.discountedPrice,
+         regularPriceBeforeDiscount: req.body.regularPriceBeforeDiscount
+      },
+
+      productAboutInfo: req.body.productAboutInfo,
+      productSpecs: req.body.productSpecs,
+   }
+
+   req.body = parsedData;
+   next();
+})
+
+exports.update = catchAsync( async (req, res, next) => {
+   let id;
+   req.query.id === undefined ? next(new AppError('id is undefined!', 400)) : id = req.query.id;
+   const data = req.body;
+   await shopModel.findByIdAndUpdate(id, data );  
+   res.status(200).json({
+      status: 'Update Successful!'
+   })
+})
