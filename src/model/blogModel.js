856649b5f4e9fa3e0690d1bad8c7d6d7d0c0bf78
Mahinul <himahinulabid@gmail.com>
2024-02-01 17:37:57 +0600

    create new blog model

---
+++ src/model/blogModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+   author : {
+      type: String,
+      required: [true, 'Author is missing!']
+   },
+   subject : {
+      type: String,
+      required : [true, 'Article subject is missing!']
+   },
+   articleTitle : {
+      type: String,
+      required: [true, 'Article title is missing!']
+   },
+   articleData : {
+      type: String,
+      required: [true, 'Article data is missing!']
+   },
+   articleImageCollection : {
+      type: Object,
+      required : [true, 'article image collection is missing!']
+   },
+   articlePublishedDate : {
+      type : Date,
+      required : [true, 'article date is mising']
+   },
+   articleEditDate : {
+      type : Object
+   }
+})
