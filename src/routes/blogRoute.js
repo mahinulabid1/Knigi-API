54264411906ebd7bdd7604c2a3f64b161966b8c5
Mahinul <himahinulabid@gmail.com>
2024-02-02 14:56:06 +0600

    add more CRUD operation routes

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('@controller/blogController/authController');
 const newArticleController = require('@controller/blogController/createArticle')
+const fetchData = require('@controller/blogController/fetchBlogData')
 const cloudinaryController = require('@cloudinary/controller')
+const update = require('@controller/blogController/updateArticle')
+const deleteQuery = require('@controller/blogController/deleteArticle')
 
 router.post(
    '/newArticle', 
@@ -15,5 +18,26 @@ router.post(
    newArticleController.createNewArticle
 )
 
+router.get('/allArticle', fetchData.fetchAllArticle);
+router.get('/allArticle/page/:pageNumber', fetchData.fetchArticleWithPignation)
+
+
+router.patch(
+   '/article/id/:idValue',
+   authController.protectBadInputRequest,
+   update.updateMongodbData
+);
+
+router.delete(
+   '/article/id/:idValue',
+   deleteQuery.articleValidation,
+   deleteQuery.fetchImagePublicId,
+   cloudinaryController.deleteMultipleImage,
+   deleteQuery.deleteArticle
+)
+
 
 module.exports =  router;
+
+// router.get('/allArticle/limit/:limitValue');
+// in this version, the update image in article will not be DEVELOPED
