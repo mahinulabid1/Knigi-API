98e33bfe137e6fe0609dbda3e258df4837f264ef
Mahinul <himahinulabid@gmail.com>
2024-02-06 01:10:08 +0600

    add dev & production state based error handler

--- src/controller/errorController.js
+++ src/controller/errorController.js
@@ -1,11 +1,23 @@
-const AppError = require('@utils/appError.js');
+const AppError = require('@utils/appError');
+const mongoose = require('mongoose')
+const cloudinaryController = require('@src/cloudinaryStorage/controller');
 const dotenv = require('dotenv')
-dotenv.config({path: '@dotenv'});
+dotenv.config({ path: '@dotenv' });
 
 const nodeEnvironment = process.env.NODE_ENV;
 
-const reverseUpload = () => {
+const uploadRollback = async ( req ) => {
    // delete Uploaded files
+   const isUploadPerformed = req.isFileUploaded.status; // true or false
+   const filePublicIdArr = req.isFileUploaded.publicIdArr;
+
+   if(isUploadPerformed) {
+      for(let i = 0; i < filePublicIdArr.length; i++ ) {
+         await cloudinaryController.deleteImage(filePublicIdArr[0])
+         console.log(`File Upload RollBack SuccesFul! Public ID ${filePublicIdArr[0]}`);
+      }
+   }
+   
 }
 
 const castErrorHandler = (err, req, res) => {
@@ -14,45 +26,87 @@ const castErrorHandler = (err, req, res) => {
    return new AppError(message, 400);
 }
 
+const duplicateKeyHandler = err => {
+   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+   const message = `Duplicate field value: ${value}. Please use another value!`;
+   return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+   console.log('running validation error');
+   const errors = Object.values(err.errors).map(el => el.message);
+ 
+   const message = `Invalid input data. ${errors.join('. ')}`;
+   return new AppError(message, 400);
+ };
+ 
+
+const handleJWTError = () => {
+   new AppError('Invalid token. Please log in again!', 401);
+}
+
+const handleJWTExpiredError = () => {
+   new AppError('Your token has expired! Please log in again.', 401);
+}
+
 const sendDevError = (err, req, res) => {
-   return res.status(err.statusCode).json({
-      status : err.statusCode,
-      message : err.message,
-      stack: err.stack
-   })
+   if (req.originalUrl.startsWith('/api')) {
+      return res.status(err.statusCode).json({
+         status: err.status,
+         error: err,
+         message: err.message,
+         stack: err.stack
+      });
+   }
 }
 
-const sendProdError = ( err, req, res ) => {
-
-}
+const sendErrorProd = (err, req, res) => {
+   // A) API
+   if (req.originalUrl.startsWith('/api')) {
+     // A) Operational, trusted error: send message to client
+     if (err.isOperational) {
+       return res.status(err.statusCode).json({
+         status: err.status,
+         message: err.message
+       });
+     }
+     // B) Programming or other unknown error: don't leak error details
+     // 1) Log error
+     console.error('ERROR 💥', err);
+     // 2) Send generic message
+     return res.status(500).json({
+       status: 'error',
+       message: 'Something went very wrong!'
+     });
+   }
+ 
+ };
 
 // main global error handling middleware
 module.exports = (err, req, res, next) => {
+   uploadRollback(req);
    err.statusCode = err.statusCode || err.status || 500;
    err.message = err.message || 'error';
 
- 
-   if(nodeEnvironment === 'development') {
-      console.log("this is development error")
+
+   if (nodeEnvironment === 'development') {
       console.log(err);
       sendDevError(err, req, res);
-   } else {
-      let error = {...err}
-      if(err.name === "CastError") error = castErrorHandler(err);
-      res.status(error.statusCode).json({
-         status : "error",
-         message: error.message,
-         name: error.name,
-         errData : {
-            path : err.path,
-            value: err.value
-         }
-      })
    }
-   
+   else {
+      let error = { ...err }
+      const isValidationErr = err instanceof mongoose.Error.ValidationError;
+      console.log(err instanceof mongoose.Error.ValidationError);
+      // console.log(error)
+      if (error.name === 'CastError') {error = castErrorHandler(error); }
+      if (error.code === 11000) { error = duplicateKeyHandler(error);}
+      if (isValidationErr){
+         error = handleValidationErrorDB(error);
+      }
+      if (error.name === 'JsonWebTokenError') error = handleJWTError();
+      if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+      sendErrorProd(error, req, res);
+   }
 
-  
-
-
-   
 }
